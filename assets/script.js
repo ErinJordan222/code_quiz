50a41eaf812f4ce74b65ec98385ed099111974ee
ErinJordan222 <erin.jordan@du.edu>
2022-05-13 23:13:37 -0600

    started_html_css_javascript_questions

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+const questions = [
+
+    { 
+        question: 'Which HTML element do we put in for JavaScript?',
+        answers: [
+        {option: '1. javascript', answer: false},
+        {option: '2. script', answer: true},
+    ]
+    
+    },
+
+    { question: 'How do you create a function in JavaScript?',
+     answers: [
+         {option: '1. Function = My Function()', answer: false},
+         {option: '2. function = myFunction()', answer: true},
+     ]
+
+
+     },
+
+     { question: 'Which of these are considered Javascript data types?', 
+
+     answers: [
+      { option: '1. String, Number, Boolean', answer: true},
+      {option: '2. Buttons, ABC, Bolognese', answer: false},
+     ]
+
+
+     },
+
+     {question: 'How do you declare a JavaScript variable?', 
+      answers: [
+     {option: '1. var colorName', answer: true},
+      {option: '2. variableColorName', answer: false},
+    ]
+
+
+     },
+
+     {question: 'What is the correct way to write a javascript array?', 
+      answers: [
+     {option: '1. var animals =["dog", "cat", "bird"]', answer: true},
+      {option: '2. var animals = "dog", "cat", "bird"', answer: false},
+    ]
+
+
+     },
+
+     {question: 'Bonus Question: In javascript 1 = ___',
+      answers: [
+     {option: '1. 1', answer: false},
+      {option: '2. 0', answer: true},
+    ]
+     }
+
+
+];
